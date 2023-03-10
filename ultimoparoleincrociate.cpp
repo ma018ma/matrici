60b8b33e991428cba842abd80cb618ae98750dd6
ma018ma <marzia018@libero.it>
2023-03-10 13:22:22 +0100

    diagonale

--- ultimoparoleincrociate.cpp
+++ ultimoparoleincrociate.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int rows = 10;
     int columns = 10;
-    string words[] = {"quattro", "cinque", "dieci", "treno", "tu", "voi"};
+    string words[] = {"lei", "tuo", "gas", "ape", "tre", "gia"};
     char empty = ' ';
 
     int wordsLength = sizeof(words) / sizeof(words[0]);
@@ -94,9 +94,9 @@ int main()
                 for (int i = 0; i < words[index].length(); i++)
                 {
                     matrix[x + i][y] = words[index][i];
-                    // cout << "index: " << index << " i: " << i << " x: " << x << " y: " << y << endl;
-                    // cout << "matrix[" << x << "][" << y + i << "] = " << words[index][i] << endl;
-                    // cout << "result: " << matrix[x][y + i] << endl;
+                     cout << "index: " << index << " i: " << i << " x: " << x << " y: " << y << endl;
+                     cout << "matrix[" << x << "][" << y + i << "] = " << words[index][i] << endl;
+                     cout << "result: " << matrix[x][y + i] << endl;
                 }
             }
         }
@@ -112,15 +112,17 @@ int main()
                 contatore++;
                 int x = rand() % (rows - words[index].length());
                 int y = rand() % (columns - words[index].length());
-
+                //x==y;
                 while (matrix[x +1][y +1] != empty)
-                {
+                { 
+                
                     x = rand() % (rows - words[index].length());
                     y = rand() % (columns - words[index].length());
+              
                 }
 
-                for (int i = 0; i < words[index].length(); i++)
-                {
+                for (int i = 0; i < words[index].length()-1; i++)
+                {   
                     if (matrix[x + i][y + i] != empty)
                     {
                         x = rand() % (rows - words[index].length());
@@ -131,11 +133,11 @@ int main()
 
                 for (int i = 0; i < words[index].length(); i++)
                 {
-                    x==y;
-                    matrix[i+y][i+x] = words[index][i];
-                   // cout << "index: " << index << " i: " << i << " x: " << x << " y: " << y << endl;
-                    //cout << "matrix[" << x << "][" << y + i << "] = " << words[index][i] << endl;
-                    //cout << "result: " << matrix[x][y + i] << endl;
+                    
+                    matrix[x+i][y+i] = words[index][i];
+                    cout << "index: " << index << " i: " << i << " x: " << x << " y: " << y << endl;
+                    cout << "matrix[" << x << "][" << y + i << "] = " << words[index][i] << endl;
+                    cout << "result: " << matrix[x][y + i] << endl;
                 }
             }
         }
